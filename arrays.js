a8deb01e3da60630846be02364525c1105cdef1d
Anupama Gadde <anupamagadde@yahoo.com>
2019-11-18 17:12:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
-  return array = [element, ...array];
+  return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element);
 }
 function addElementToEndOfArray(array, element) {
-  return array = [...array, element];
+  return  [...array, element];
 }
 function destructivelyAddElementToEndOfArray(array, element) {
   return array.push(element);
