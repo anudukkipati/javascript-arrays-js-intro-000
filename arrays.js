051f4962e02370994392267d36ffde742819d0f7
Anupama Gadde <anupamagadde@yahoo.com>
2019-11-18 17:11:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,15 +1,15 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
-  array = [element, ...array];
+  return array = [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
+  return array.unshift(element);
 }
 function addElementToEndOfArray(array, element) {
-  array = [...array, element];
+  return array = [...array, element];
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
+  return array.push(element);
 }
 function accessElementInArray(array, index) {
   return array[index];
@@ -18,11 +18,11 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
   return array.shift(element);
 }
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
+  return array.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop();
+  return array.pop();
 }
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1);
+  return array.slice(0, array.length - 1);
 }
